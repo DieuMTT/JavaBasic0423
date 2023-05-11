d67cb8f65a83216fdc64a0b2a4db1763b778065d
DieuMTT <maithithuydieu1@gmail.com>
2023-05-11 14:16:44 +0900

    CommitBT4

---
+++ src/BT4/VongLapForVaMang.java
@@ -0,0 +1,22 @@
+package BT4;
+
+public class VongLapForVaMang {
+    public static void main(String[] args) {
+        //Tạo mảng để lưu số chẵn từ 0-50
+        int mang1[] = new int[51];
+        //Tạo vòng lặp for để in ra số chẵn
+        System.out.println("In ra số chẵn 0-50 trước khi nạp vào mảng");
+        for (int i = 0; i <= 50; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+                mang1[i] = i; //Nạp số chẵn vào mảng
+            }
+        }
+        //Duyệt mảng để in ra kết quả sau khi nạp vào
+        System.out.println("In ra số chẵn 0-50 sau khi nạp vào mảng");
+        for (int i = 0; i < mang1.length; i++) {
+            System.out.println(mang1[i]);
+        }
+
+    }
+}
