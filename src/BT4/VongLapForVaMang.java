4bde59e3a5aab87cae66cb30033d9e0e0ebfbc93
DieuMTT <maithithuydieu1@gmail.com>
2023-05-11 17:38:28 +0900

    Commit BT4

--- src/BT4/VongLapForVaMang.java
+++ src/BT4/VongLapForVaMang.java
@@ -2,21 +2,23 @@ package BT4;
 
 public class VongLapForVaMang {
     public static void main(String[] args) {
-        //Tạo mảng để lưu số chẵn từ 0-50
-        int mang1[] = new int[51];
-        //Tạo vòng lặp for để in ra số chẵn
-        System.out.println("In ra số chẵn 0-50 trước khi nạp vào mảng");
+        // Tạo mảng để lưu những số chẵn từ 0 đến 50
+        int MangSoChan[] = new int[26];
+        int j = 0; // Biến để gán vị trí cho MangSoChan
+
+        //Tạo vòng lặp FOR để in ra các số chẵn từ 0 đến 50
+        System.out.println("Vòng lặp FOR để in ra các số chẵn từ 0 đến 50:");
         for (int i = 0; i <= 50; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
-                mang1[i] = i; //Nạp số chẵn vào mảng
+                MangSoChan[j] = i;
+                j++;
             }
         }
         //Duyệt mảng để in ra kết quả sau khi nạp vào
-        System.out.println("In ra số chẵn 0-50 sau khi nạp vào mảng");
-        for (int i = 1; i < mang1.length; i++) {
-            System.out.println(mang1[i]);
+        System.out.println("In ra các số chẵn từ 0 đến 50 sau khi nạp vào mảng:");
+        for (int i = 0; i < MangSoChan.length; i++) {
+            System.out.println(MangSoChan[i]);
         }
-
     }
 }
